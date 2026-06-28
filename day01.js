96d6083363ee2f68dfad3a50a3bac80badfa8510
M Ahmad <haalim408@gmail.com>
2026-06-28 23:19:35 +0500

    Day 1: first JavaScript variables exercise

---
+++ day01.js
@@ -0,0 +1,22 @@
+// console.log("Hello world")
+// let myName =  "M Ahmad"
+// let myAge = 22;
+// let myCity = "Lahore"
+// const myCountry = "Pakistan"
+// let isStudent = true;
+
+// console.log(myName)
+// console.log(myAge)
+// console.log(myCity)
+// console.log(myCountry)
+// console.log(isStudent)
+
+let myName =  "M Ahmad"
+let myAge = 20;
+const myCountry = "Pakistan"
+let isChai = true;
+
+console.log(myName)
+console.log(myAge)
+console.log(myCountry)
+console.log(isChai)
