69c79b680249ede8fa0dfabe227d8ed0bb658dcb
M Ahmad <haalim408@gmail.com>
2026-07-09 23:07:54 +0500

    Day 02: numbers,operators, Shopping calculator

--- day02.js
+++ day02.js
@@ -1,15 +1,46 @@
-let name = " M Ahmad"
-let name2 = 'M Ahmad'
-let name3 = `M Ahmad`
+// let name = " M Ahmad"
+// let name2 = 'M Ahmad'
+// let name3 = `M Ahmad`
 
-let myName = " M Ahmad";
-let city = "Lahore";
-let age = 20;
+// let myName = " M Ahmad";
+// let city = "Lahore";
+// let age = 20;
 
-console.log(myName.length)
-console.log(myName.toUpperCase());
-console.log(myName.toLowerCase());
-console.log(myName.includes("Ahmad"))
-console.log(myName.trim().length)
+// console.log(myName.length)
+// console.log(myName.toUpperCase());
+// console.log(myName.toLowerCase());
+// console.log(myName.includes("Ahmad"))
+// console.log(myName.trim().length)
 
-console.log(`My name is ${myName} I live in ${city}`)
+// console.log(`My name is ${myName} I live in ${city}`)
+
+// let itemPrice = 1500;
+// let quantity = 3;
+// let discount  = 0.10;
+// let deliveryFee = 200;
+
+// let totalPrice = itemPrice*quantity;
+// let discountPrice = totalPrice*discount;
+// let finalPrice = totalPrice - discountPrice + deliveryFee;
+
+// console.log(`Item Price : ${itemPrice}`)
+// console.log(`Quantity ${quantity}`)
+// console.log(`Total ${totalPrice}`)
+// console.log(`Discount ${discountPrice}`)
+// console.log(`Final Price ${finalPrice}`)
+
+
+//Exercise
+
+let ticketPrice  = 5000;
+let passengers  = 4;
+let tax   = 0.15;
+
+let totalPrice = ticketPrice*passengers;
+let taxAmount = totalPrice*tax;
+let finalTotal = totalPrice + taxAmount;
+
+console.log(`ticket Price : ${ticketPrice}`)
+console.log(totalPrice)
+ console.log(`Tax Amount ${taxAmount}`)
+// console.log(`Final Total ${finalTotal}`)
