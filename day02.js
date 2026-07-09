cd636e438ec3ab54373084e197a44dc1992c7d0a
M Ahmad <haalim408@gmail.com>
2026-07-09 22:35:58 +0500

    Day 02 : Strings , Literals , String methods

---
+++ day02.js
@@ -0,0 +1,15 @@
+let name = " M Ahmad"
+let name2 = 'M Ahmad'
+let name3 = `M Ahmad`
+
+let myName = " M Ahmad";
+let city = "Lahore";
+let age = 20;
+
+console.log(myName.length)
+console.log(myName.toUpperCase());
+console.log(myName.toLowerCase());
+console.log(myName.includes("Ahmad"))
+console.log(myName.trim().length)
+
+console.log(`My name is ${myName} I live in ${city}`)
